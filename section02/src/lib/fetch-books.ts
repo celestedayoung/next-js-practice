66590f541d48ac787d79bf6264b40446b2f5c251
dayoungpyo <dayoungpyo@gmail.com>
2024-09-13 15:47:59 +0900

    feat: 각 페이지 SSR로 변경

---
+++ section02/src/lib/fetch-books.ts
@@ -0,0 +1,22 @@
+import { BookData } from "@/types";
+
+// q?를 매개변수로 전달해 선택적으로 받을 수 있게 한다.
+export default async function fetchBooks(q?: string): Promise<BookData[]> {
+  let url = `http://localhost:12345/book`;
+
+  // q가 있으면 url을 검색 url로 바꿔준다.
+  if (q) {
+    url += `/search?1=${q}`;
+  }
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error();
+    }
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,21 +1,41 @@
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+// SSR로 만드는 함수: 컴포넌트 보다 먼저 실행되어서 컴포넌트에 필요한 데이터를 불러오는 함수
+// 1. 데이터를 미리 불러온다.
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: { allBooks, recoBooks },
+  };
+};
+
+// 2. 페이지 컴포넌트가 실행된다.
+// 서버에서 한 번, 수화 과정에서 한 번, 총 두 번 실행된다.
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
