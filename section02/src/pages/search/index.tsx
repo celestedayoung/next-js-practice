bed32cca26e426c3097c3b92ff37a6ba15630267
dayoungpyo <dayoungpyo@gmail.com>
2024-09-13 13:44:15 +0900

    feat: UI 구현

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,16 +1,16 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-  // useRouter: router라는 객체를 컴포넌트 내부에서 사용할 수 있도록 반환해주는 훅
-  console.log(router);
-  // console이 2번 찍히는 이유: query string을 읽는 과정 중에 렌더링을 한 번 더 하기 때문
-
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
