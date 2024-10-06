fce51949eeb12e50dfaca444c909b0c8072a0c14
celestedayoung <dayoungpyo@gmail.com>
2024-10-06 15:48:25 +0900

    feat: 재검증 구현

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
@@ -20,7 +20,22 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     // next server에게 해당 페이지를 재생성 해달라 요청하는 메서드
-    revalidatePath(`/book/${bookId}`);
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // 2. 특정 경로의 모든 동적 페이지를 재검증 (폴더 이름을 명시하는 것)
+    // revalidatePath("/book/[id]", "page");
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지를 재검증
+    // revalidatePath("/(with-search-bar)", "layout");
+
+    // 4. 모든 데이터를 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. tag 값을 기준으로 데이터 캐시를 재검증 (fetch에 tag 방법을 말하는 것)
+    // ex) await fetch('....', { next: tags: [`review-${bookId}`]})
+    revalidateTag(`review-${bookId}`);
+
     console.log(response.status);
   } catch (err) {
     console.error(err);
